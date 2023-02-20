3be85ce518a08fcaa5cf978db3532e53a2d1894a
arkal <arkal@192.168.1.8>
2023-02-19 21:46:29 -0500

    ancle tag added

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './game.css'
 
-const Game = ({icon, alt, title, className}) => {
-  return <div className="game__container">
+const Game = ({href, icon, alt, title, className}) => {
+  return <a href={href} target="_blank" rel="noopener noreferrer" className="game__container">
 		<img src={icon} alt={alt} className={`icon__game ${className}`}  />
 		<h2 className='title__game' >{title}</h2>
-	</div>;
+	</a>;
 };
 
 export default Game;
--- src/components/selectGame/SelectGame.jsx
+++ src/components/selectGame/SelectGame.jsx
@@ -6,12 +6,15 @@ import iconCallOfDuty from '../../images/CallOfDuty.jpg'
 import iconLeagueOfLegends from '../../images/LeagueofLegends.png'
 
 const SelectGame = () => {
+  const redireccionar = ()=>{
+    window.open('https://www.google.com.ar/', '_blank')
+  }
   return (
     <>
       <div className="select__game__container">
-        <Game icon={iconFree} alt='icon Free Fire' title='Free Fire' />
-        <Game icon={iconCallOfDuty} alt='icon Free Fire' title='Call of Duty: Mobile' className='border__game__image' />
-        <Game icon={iconLeagueOfLegends} alt='icon Free Fire' title='League of Legends (Latinoamérica)' className='border__game__image' />
+        <Game href={"https://pagostore.com/app"} icon={iconFree} alt='icon Free Fire' title='Free Fire' onClick={redireccionar} />
+        <Game href={"https://www.callofduty.com/mobile"} icon={iconCallOfDuty} alt='icon Free Fire' title='Call of Duty: Mobile' className='border__game__image' />
+        <Game href={"https://www.leagueoflegends.com/es-es/"}  icon={iconLeagueOfLegends} alt='icon Free Fire' title='League of Legends (Latinoamérica)' className='border__game__image' />
       </div>
     </>
   );
